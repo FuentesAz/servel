dca4f7cc66cfec24b6b0be33fa6faf5b2e46e709
zared <zared@example.com>
2026-01-06 12:55:42 -0500

    Se agrega barra de navegacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+  },
   vite: {
+    server:{
+      allowedHosts: ['.ngrok-free.app',]
+    },
     plugins: [tailwindcss()]
   },
   site: 'https://servel.com'
