d5ca3a3376d5a3f40482500fe5eeb89645ef1acc
Your Name <you@example.com>
2026-02-16 20:42:12 -0500

    fix:verificacion de formato en endpoint

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -5,19 +5,24 @@ import { resend } from "../../lib/resend";
 export const POST: APIRoute = async ({ request }) => {
 
     const contentType = request.headers.get("Content-Type");
+    let name, email, phone, message;
 
-    if (!contentType || !contentType.includes("multipart/form-data")) {
-        return new Response(
-            JSON.stringify({ error: `Esperaba form-data pero recibí: ${contentType}` }), 
-            { status: 400 }
-        );
+    // Detectar si es JSON o Form Data
+    if (contentType?.includes("application/json")) {
+        const body = await request.json();
+        name = body.name;
+        email = body.email;
+        phone = body.phone;
+        message = body.message;
+    } else if (contentType?.includes("multipart/form-data")) {
+        const data = await request.formData();
+        name = data.get("name")?.toString();
+        email = data.get("email")?.toString();
+        phone = data.get("phone")?.toString();
+        message = data.get("message")?.toString();
+    } else {
+        return new Response(JSON.stringify({ error: "Formato no soportado" }), { status: 400 });
     }
-
-    const data = await request.formData();
-    const name = data.get("name")?.toString().trim();
-    const email = data.get("email")?.toString().trim();
-    const phone = data.get("phone")?.toString().trim();
-    const message = data.get("message")?.toString().trim();
 
     if (!name || !email || !message) {
         return new Response(JSON.stringify({ success: false, error: 'Todos los campos son obligatorios' }), { status: 400 })
